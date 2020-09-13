6eddbe89a2cfb20e497fe3185ae72a9f542efe0e
vungocson <vungocson998@gmail.com>
2020-09-13 16:23:02 +0700

    [master] print n last lines.

--- main.go
+++ main.go
@@ -2,25 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	n, _ := strconv.Atoi(os.Args[1])
 
-	f, e := os.OpenFile("./files/demo.txt", os.O_CREATE|os.O_RDWR, 0777)
+	fmt.Printf("Last %d lines :\n\n", n)
 
-	if e != nil {
-		panic(e)
+	var num int
+	var i int64
+	var prevIsBreak bool = true
+	buffer := make([]byte, 1)
+
+	f, _ := os.OpenFile("./files/demo.txt", os.O_RDONLY, 0777)
+
+	f.Seek(0, 2)
+
+	num = 0
+	i = 0
+
+	for num < n {
+		f.Seek(-i, 2)
+		f.Read(buffer)
+
+		if buffer[0] == byte(10) || buffer[0] == byte(13) {
+			if prevIsBreak {
+
+			} else {
+				num++
+				prevIsBreak = true
+			}
+		} else {
+			prevIsBreak = false
+		}
+		i++
 	}
 
-	for i := 0; i < 1000; i++ {
-		s := "This is line " + strconv.Itoa(i+1) + "\n"
-
-		f.WriteString(s)
+	for true {
+		_, e := f.Read(buffer)
+		if e == io.EOF {
+			break
+		} else {
+			fmt.Printf("%s", buffer)
+		}
 	}
 
-	f.Close()
+	fmt.Printf("\n")
 
 }
