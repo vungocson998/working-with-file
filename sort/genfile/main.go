643c11f582802afcbd9ee3861425c581ea496379
vungocson998 <vungocson998@gmail.com>
2020-09-14 10:11:38 +0700

    [master] sort/genfile

---
+++ sort/genfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, _ := os.OpenFile("../../files/input.txt", os.O_CREATE|os.O_RDWR, 0777)
+
+	var s string
+
+	for i := 0; i < 10; i++ {
+		s = strconv.Itoa(rand.Intn(20000)) + "\t"
+		f.WriteString(s)
+	}
+
+	f.Close()
+	fmt.Printf("Exported file ../files/input.txt\n")
+}
