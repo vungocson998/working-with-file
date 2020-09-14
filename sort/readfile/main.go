18bc7afde36df8a60166e1397554e3948125f937
vungocson998 <vungocson998@gmail.com>
2020-09-14 16:01:54 +0700

    [master] sorted

--- sort/readfile/main.go
+++ sort/readfile/main.go
@@ -2,48 +2,89 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
+	"io/ioutil"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 
-	numbers := readFile("../../files/input.txt")
-	fmt.Printf("Array of %d numbers :\n%v\n", len(numbers), numbers)
-
-	sort.Ints(numbers)
-	fmt.Printf("Sorted array: %v\n", numbers)
+	mySort("../../files/input.txt")
 
 	elapsed := time.Since(start)
 	fmt.Printf("Time: %s\n", elapsed)
 }
 
-func readFile(filePath string) (numbers []int) {
-	f, e := os.OpenFile(filePath, os.O_RDONLY, 0777)
+func readFile(filePath string, start int, end int) (numbers []int) {
+	data, _ := ioutil.ReadFile(filePath)
 
-	defer f.Close()
+	numbersStr := strings.Split(string(data), "\t")
 
-	if e != nil {
-		panic(e)
+	for _, v := range numbersStr {
+		vInt, _ := strconv.Atoi(v)
+		numbers = append(numbers, vInt)
 	}
 
-	var num int
+	return numbers
+}
 
-	for {
-		_, err := fmt.Fscanf(f, "%d\t", &num)
+func splitNumArr(numbers []int) (left, right []int) {
+	k := len(numbers)
+	left = numbers[0 : k/2]
+	right = numbers[k/2+1:]
 
-		if err != nil {
-			fmt.Println(err)
-			if err == io.EOF {
-				return
-			}
-			panic(fmt.Sprintf("Scan Failed %s: %v", filePath, err))
+	return left, right
+}
 
+func merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+	count := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			slice[count] = left[i]
+			count, i = count+1, i+1
+		} else {
+			slice[count] = right[j]
+			count, j = count+1, j+1
 		}
-		numbers = append(numbers, num)
 	}
+	for i < len(left) {
+		slice[count] = left[i]
+		count, i = count+1, i+1
+	}
+	for j < len(right) {
+		slice[count] = right[j]
+		count, j = count+1, j+1
+	}
+
+	return slice
+}
+
+func partSort(nums []int) (slice []int) {
+	sort.Ints(nums)
+
+	slice = nums
+
+	return
+}
+
+func mySort(filePath string) (slice []int) {
+
+	numbers := readFile("../../files/input.txt", 0, 5)
+
+	left, right := splitNumArr(numbers)
+
+	left = partSort(left)
+	right = partSort(right)
+
+	slice = merge(left, right)
+
+	fmt.Printf("Sorted array: %v\n", numbers)
 	return
 }
