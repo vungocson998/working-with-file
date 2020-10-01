c8f3101f87174c0eec859ab2cbb099617004bab4
Son Vu <vungocson998@gmail.com>
2020-10-01 08:39:51 +0700

    Add flag to genfile

---
+++ sort/genfile/README.md
@@ -0,0 +1,12 @@
+# Build ./genfile
+
+go build .
+
+# Usage of ./genfile:
+
+Generate n random integer numbers greater than zero to a file, separated by "\t"
+
+  -file string
+    	where you want to save your file (default "../../files/input.txt")
+  -n int
+    	amount of numbers you want to generate to the file (default 10000)
--- sort/genfile/main.go
+++ sort/genfile/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -8,15 +9,22 @@ import (
 )
 
 func main() {
-	f, _ := os.OpenFile("../../files/input.txt", os.O_CREATE|os.O_RDWR, 0777)
+	filePath := flag.String("file", "../../files/input.txt", "where you want to save your file")
+	num := flag.Int("n", 10000, "amount of numbers you want to generate to the file")
+	flag.Parse()
+	generateFile(*filePath, *num)
+}
 
+// Generate random numbers which are separated by "\t"
+func generateFile(filePath string, num int) {
+	f, _ := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0777)
+	defer f.Close()
 	var s string
 
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < num; i++ {
 		s = strconv.Itoa(rand.Intn(20000)) + "\t"
 		f.WriteString(s)
 	}
 
-	f.Close()
-	fmt.Printf("Exported file ../files/input.txt\n")
+	fmt.Printf("Exported file %s\n", filePath)
 }
