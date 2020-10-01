8b383240b700ab0b9442ca19711afcbc39c26d0e
Vu Son <sonvn@smartosc.com>
2020-10-01 18:18:50 +0700

    Eearly return

--- tailer/main.go
+++ tailer/main.go
@@ -7,13 +7,16 @@ import (
 	"os"
 )
 
+const (
+	EOL_CHAR_1 = byte('\n')
+	EOL_CHAR_2 = byte('\r')
+)
+
 func main() {
 	filePath := flag.String("file", "../files/receive.txt", "path to the file")
 	n := flag.Int("n", 1, "number of last lines you want to print out")
 	flag.Parse()
-
 	tailer(*n, *filePath)
-
 }
 
 func tailer(n int, filePath string) {
@@ -24,7 +27,6 @@ func tailer(n int, filePath string) {
 	var prevIsBreak bool = true // Used to detect \r\n or \n\r cases
 	buffer := make([]byte, 1)
 
-	// Open and move fseek to the end of file
 	f, _ := os.OpenFile(filePath, os.O_RDONLY, 0777)
 	defer f.Close()
 	f.Seek(0, 2)
@@ -36,27 +38,24 @@ func tailer(n int, filePath string) {
 	for num < n {
 		f.Seek(-i, 2)
 		f.Read(buffer)
-		if buffer[0] == byte(10) || buffer[0] == byte(13) {
-			// Used to detect \r\n or \n\r cases
-			if prevIsBreak {
-
-			} else {
-				num++
-				prevIsBreak = true
-			}
-		} else {
+		if buffer[0] != EOL_CHAR_1 && buffer[0] != EOL_CHAR_2 {
 			prevIsBreak = false
+			i++
+			continue
+		}
+		if !prevIsBreak {
+			num++
+			prevIsBreak = true
 		}
 		i++
 	}
 	// Print n lines
-	for true {
+	for {
 		_, e := f.Read(buffer)
 		if e == io.EOF {
 			break
-		} else {
-			fmt.Printf("%s", buffer)
 		}
+		fmt.Printf("%s", buffer)
 	}
 	fmt.Printf("\n")
 }
